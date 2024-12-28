3fdad3c8a79ec11e9652b9cbef473554a0790c28
giackperetti <github.giack@gmail.com>
2024-12-28 19:07:03 +0100

    completed all sections

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -5,10 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+use App\Events\ChirpCreated;
+
 class Chirp extends Model
 {
     protected $fillable = [
         'message',
+    ];
+
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
     ];
 
     public function user(): BelongsTo
